7585ec1aec4cee0bba8108456d5828f6403fe0dd
ccofrev <carlos.cofre@usach.cl>
2024-06-03 04:35:37 -0400

    Oculta demo bitcoin

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React API Demo</h1>
-        <DataFetcher />
+        <h1>Test</h1>
+        {/* <DataFetcher /> */}
 	      <MqttSubscriber />
       </header>
     </div>
